5f2b88894e7f676f71109742cfda61f62359636b
kevin-murithi <murithikevin54@gmail.com>
2024-09-22 16:01:07 +0300

    resources section

--- models/db.js
+++ models/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '#Secure1234',
+    password: process.env.DB_PASSWORD || '0712400421',
     database: process.env.DB_NAME || 'foodwaste'
 });
 
